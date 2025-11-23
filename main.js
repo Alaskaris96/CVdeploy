aefb3b7a819e1c0af906ece4d6d231d8fdc744ba
Andreas Laskaris <61242095+Alaskaris96@users.noreply.github.com>
2025-11-24 01:02:30 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+import puppeteer from "puppeteer";
+
+const generatePDF = async () => {
+    const browser = await puppeteer.launch({
+        args: ['--font-render-hinting=none']
+    });
+    const page = await browser.newPage();
+
+    await page.goto(`file://${process.cwd()}/index.html`, {
+        waitUntil: "networkidle0",
+    });
+
+    await page.pdf({
+        path: "cv.pdf",
+        format: "A4",
+        printBackground: true,
+        preferCSSPageSize: true,
+        scale: 0.77,
+    });
+
+    await browser.close();
+};
+
+generatePDF();
